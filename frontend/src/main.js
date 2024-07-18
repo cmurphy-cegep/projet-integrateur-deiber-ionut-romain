33683c19dd0b28ed1e7e6090fddebccd2571c411
ionut8891 <148498333+ionut8891@users.noreply.github.com>
2024-07-18 10:04:14 -0400

    frontend_config

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,23 @@
-import './assets/main.css'
-
+import { createRouter, createWebHistory } from 'vue-router';
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+// Déclaration de Vue Router
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+       /* { path: '', component: PageAchats },
+        { path: '/recettes/:id', component: ItemCatalogueDetail, props: true },
+        { path: '/checkout', component: PageCheckout },
+        { path: '/admin/new-product', component: NewItemCatalogue },
+        { path: '/login', component: FormulaireLogin }*/
+    ]
+});
+
+// Ajout de Vue Router à l'application
+app.use(router);
+
+app.mount("#app");
+
