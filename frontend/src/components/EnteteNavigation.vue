d18362744881ad83d37f897f56333d1ba6134cde
Dnino18 <122505784+Dnino18@users.noreply.github.com>
2024-08-01 16:40:04 -0400

    Code de base entete et pied

---
+++ frontend/src/components/EnteteNavigation.vue
@@ -0,0 +1,33 @@
+<template>
+    <header>
+        <div class="container">
+            <div class="title">
+                <h1>Les Reccettes de Rodrigo</h1>
+            </div>            
+        </div>
+        <div class="nav">
+            <span><Router-link to="/">Accueil</Router-link></span>
+        </div>
+        <div class="connexion">
+            <div v-if="session.user">
+                    <div>Bienvenue, {{ session.user.userFullName }}</div>
+                    <div v-if="session.user.isAdmin" class="admin">ADMINISTRATEUR</div>
+                    <div><a href="" @click.prevent="session.disconnect()">Déconnexion</a></div>
+                </div>
+            <div v-else><router-link to="/login">Se connecter</router-link></div>
+        </div>
+        
+    </header>
+</template>
+
+<script>
+import session from '../session';
+
+export default {
+    data: function() {
+        return {
+            session: session
+        };
+    }
+}
+</script>
