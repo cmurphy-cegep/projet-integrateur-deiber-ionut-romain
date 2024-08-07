30f583dfe6eb6bd7985713cf29d60dcfc7244239
Romain Billot <206266704@cegepsherbrooke.qc.ca>
2024-08-07 10:56:41 -0400

    F05 requis check credentials route fail

--- backend/tests/signupRoute.test.js
+++ backend/tests/signupRoute.test.js
@@ -5,6 +5,50 @@ jest.mock('../src/queries/userAccountQueries');
 const userAccountQueries = require('../src/queries/userAccountQueries');
 
 describe('Test signup route', () => {
+	it('with inexistant username should return code 400', async () => {
+		const bodyReq = {
+			username: null,
+			password: 'password',
+			fullname: 'fullname'
+		};
+
+		return request(app)
+			.post('/signup')
+			.send(bodyReq)
+			.expect(400)
+			.then(response => {
+				expect(response.body.message).toEqual('Le champ username est requis');
+			});
+	});
+	it('with inexistant password should return code 400', async () => {
+		const bodyReq = {
+			username: 'userId',
+			password: null,
+			fullname: 'fullname'
+		};
+
+		return request(app)
+			.post('/signup')
+			.send(bodyReq)
+			.expect(400)
+			.then(response => {
+				expect(response.body.message).toEqual('Le champ password est requis');
+			});
+	});
+	it('with inexistant fullname should return code 400', async () => {
+		const bodyReq = {
+			username: 'userId',
+			password: 'password',
+			fullname: null
+		};
+		return request(app)
+			.post('/signup')
+			.send(bodyReq)
+			.expect(400)
+			.then(response => {
+				expect(response.body.message).toEqual('Le champ fullname est requis');
+			});
+	});
 	it('with successful signup should return user information in json with code 200', () => {
 		const userId = 'userId';
 		const password = 'motdepasse';
