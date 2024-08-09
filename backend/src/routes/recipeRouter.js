ddb5395f2d5a560bfc2e7750ea410131e7a9cd61
Romain Billot <206266704@cegepsherbrooke.qc.ca>
2024-08-09 09:59:00 -0400

    route upload image

--- backend/src/routes/recipeRouter.js
+++ backend/src/routes/recipeRouter.js
@@ -2,8 +2,14 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const HttpError = require("../error/HttpError");
-const RecipeServices = require("../services/RecipeServices");
 const {isValidIdSyntax} = require("../utils/utils");
+
+// Le module multer sert à gérer les téléversements (upload) de fichiers
+const multer = require('multer');
+const storage = multer.memoryStorage();
+const upload = multer({storage: storage});
+
+const RecipeServices = require("../services/RecipeServices");
 
 router.get('/', async (req, res, next) => {
 	try {
@@ -89,7 +95,7 @@ router.put('/:id',
 			return next(new HttpError(403, 'Vous n\'avez pas les permissions'));
 		}
 
-		const id = req.body.id;
+		const id = req.params.id;
 		if (!id || id === '') {
 			return next(new HttpError(400, 'L\'identifiant est requis'));
 		}
@@ -101,7 +107,7 @@ router.put('/:id',
 		try {
 			const recipe = await RecipeServices.getRecipeById(id);
 			if (!recipe) {
-				return next(new HttpError(400, `L'id ${id} ne correspond à aucune recette existante`));
+				return next(new HttpError(404, `L'id ${id} ne correspond à aucune recette existante`));
 			}
 
 			const result = await RecipeServices.updateRecipe(req.body);
@@ -110,7 +116,38 @@ router.put('/:id',
 		} catch (err) {
 			next(err);
 		}
-	}
-);
+	});
+
+router.post('/:id/image',
+	passport.authenticate('basic', {session: false}),
+	// Fonction middleware de multer pour gérer l'upload d'un fichier dans ce endpoint.
+	// Cet appel de middleware doit venir après celui de l'authentification.
+	upload.single('recipe-image'), // doit correspondre à l'id du champ dans le formulaire html
+	async (req, res, next) => {
+		if (!req.user.isAdmin) {
+			return next(new HttpError(403, 'Vous n\'avez pas les permissions'));
+		}
+
+		const id = req.params.id;
+		if (!id || id === '') {
+			return next(new HttpError(400, 'L\'identifiant est requis'));
+		}
+
+		try {
+			const recipe = await RecipeServices.getRecipeById(id);
+			if (!recipe) {
+				return next(new HttpError(404, `L'id ${id} ne correspond à aucune recette existante`));
+			}
+
+			// Le middleware upload.single(...) rendra accessible le contenu binaire du fichier
+			// téléversé dans req.file.buffer et le type de fichier (p.ex. "image/jpeg")
+			// dans req.file.mimetype:
+			await RecipeServices.updateRecipeImage(id, req.file.buffer, req.file.mimetype);
+
+			res.json("");
+		} catch (err) {
+			next(err);
+		}
+	});
 
 module.exports = router;
