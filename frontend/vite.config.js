97c9ad00698b7749d4e6048c27ba93690c35437f
Romain Billot <206266704@cegepsherbrooke.qc.ca>
2024-08-12 18:32:31 -0400

    suppression nightwatch

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,28 +1,23 @@
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from 'node:url'
 
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
-import nightwatchPlugin from 'vite-plugin-nightwatch'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    nightwatchPlugin({
-      renderPage: './nightwatch/index.html'
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      }
-    }
-  }
+	plugins: [vue()],
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url)),
+			'vue': 'vue/dist/vue.esm-bundler'
+		}
+	},
+	server: {
+		proxy: {
+			'/api': {
+				target: 'http://localhost:3000',
+				rewrite: (path) => path.replace(/^\/api/, ''),
+			}
+		}
+	}
 })
