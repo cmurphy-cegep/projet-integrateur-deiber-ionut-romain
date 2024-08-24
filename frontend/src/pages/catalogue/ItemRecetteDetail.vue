b97bb4bcde9453aa83ee8f15ae298b10fbb85f05
Romain Billot <206266704@cegepsherbrooke.qc.ca>
2024-08-24 17:48:14 -0400

    Update ItemRecetteDetail.vue

--- frontend/src/pages/catalogue/ItemRecetteDetail.vue
+++ frontend/src/pages/catalogue/ItemRecetteDetail.vue
@@ -1,18 +1,21 @@
 <template>
-	<LoadingSpinner :error="loadError" :loading="loading" />
+	<LoadingSpinner :error="loadError" :loading="loading"/>
 	<div v-if="recipe && !loading && !loadError" class="recipe">
 		<div class="recipe-name">{{ recipe.name }}</div>
-		<button type="button" v-if="session.user && session.user.isAdmin"
-			@click="$router.push({ path: `/admin/ajout-edition-item-recette/${recipe.id}` })">Éditer</button>
+		<button v-if="session.user && session.user.isAdmin" type="button"
+				@click="$router.push({ path: `/admin/ajout-edition-item-recette/${recipe.id}` })">Éditer
+		</button>
 		<div class="recipe-row">
 			<div class="recipe-image-container">
-				<img :src="imageSrc" alt="Recipe Image" class="recipe-image" />
+				<img :src="imageSrc" alt="Recipe Image" class="recipe-image"/>
 			</div>
 			<div class="recipe-description">{{ recipe.description }}</div>
 		</div>
 		<div class="recipe-row">
 			<div v-if="recipe.preparation_time" class="recipe-preparation-time">Preparation <br> {{
-				recipe.preparation_time }} minutes</div>
+					recipe.preparation_time
+				}} minutes
+			</div>
 			<div v-if="recipe.cooking_time" class="recipe-cooking-time">Cuisson <br>{{ recipe.cooking_time }} minutes
 			</div>
 			<div v-if="recipe.servings" class="recipe-servings">Portion(s) <br>{{ recipe.servings }}</div>
@@ -34,22 +37,24 @@
 			</div>
 		</div>
 		<div class="recipe-row">
-			<AppreciationsRecette :recipeId="recipe.id" />
+			<AppreciationsRecette :recipeId="recipe.id"/>
 		</div>
 		<div class="recipe-row">
-			<CommentairesRecette :recipeId="recipe.id" />
+			<CommentairesRecette :recipeId="recipe.id"/>
 		</div>
 	</div>
 </template>
 
 <script>
-import { fetchRecipe } from '../../services/recipeService.js';
-import { addApiPrefixToPath } from '../../api_utils';
+import {fetchRecipe} from '../../services/recipeService.js';
+import {addApiPrefixToPath} from '../../api_utils';
+import AppreciationsRecette from "./AppreciationsRecette.vue";
+import CommentairesRecette from './CommentairesRecette.vue';
 import LoadingSpinner from '../../components/LoadingSpinner.vue';
 import session from '../../session';
 
 export default {
-	components: { LoadingSpinner: LoadingSpinner },
+	components: {AppreciationsRecette, CommentairesRecette, LoadingSpinner},
 	props: {
 		id: String,
 		image: String
